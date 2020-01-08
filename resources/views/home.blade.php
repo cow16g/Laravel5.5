e31294b39abe2053233c0ad37ab2299b90af667d
cow16g <imamura.kazuki@gmail.com>
2020-01-08 13:57:31 +0900

    [add] 新規ユーザ登録実装
    新規ユーザ登録の実装

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset='utf-8'>
+    </head>
+    <body>
+        こんにちは！
+        @if (Auth::check())
+            {{ \Auth::user()->name }}さん
+        @else
+            ゲストさん<br>
+            <a href="/auth/register">会員登録</a>
+        @endif
+    </body>
+</html>
